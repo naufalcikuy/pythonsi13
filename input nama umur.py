ccc53afbbc48f44843438969a5c0bd6c6ae08493
naufalcikuy <bukancikuy@gmail.com>
2015-09-01 15:18:33 +0700

    first commit

---
+++ input nama umur.py
@@ -0,0 +1,4 @@
+print "SELAMAT DATANG"
+nama = raw_input("Masukkan nama anda: ")
+umur = input("Masukkan umur anda: ")
+print "Nama saya {0} dan umur saya {1} tahun".format(nama,umur)
---
+++ luas segitiga.py
@@ -0,0 +1,6 @@
+print "MENGHITUNG LUAS SEGITIGA"
+alas = input("Masukkan alas segitiga: ")
+tinggi = input("Masukkan tinggi segitiga: ")
+luas = (alas * tinggi) / 2
+print "Luas segitiga adalah {}".format(luas)
+
